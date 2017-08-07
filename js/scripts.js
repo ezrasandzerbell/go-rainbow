f9ae04ce7e4599aa187468c2a41fd4086a8ab596
ezra sandzer-bell <equilibriumarts@gmail.com>
2017-08-06 18:02:03 -0700

    refactor html by creating global var and appending to select box targets

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
+  var optionValues =
+    "<option value='black'>Black</option><option value='white'>White</option><option value='red'>Red</option><option value='orange'>Orange</option><option value='yellow'>Yellow</option><option value='green'>Green</option><option value='blue'>Blue</option><option value='purple'>Purple</option>"
+
+    $('#firstColorChoice').append(optionValues)
+    $('#secondColorChoice').append(optionValues)
+    $('#thirdColorChoice').append(optionValues)
+
   $("form#updateBackground").submit(function(event){
     event.preventDefault();
 
-    var firstColor = $("select.firstColorChoice").val();
-    var secondColor = $("select.secondColorChoice").val();
-    var thirdColor = $("select.thirdColorChoice").val();
-
-    console.log("color 1", firstColor, "color 2", secondColor, "color 3", thirdColor);
+    var firstColor = $("select#firstColorChoice").val();
+    var secondColor = $("select#secondColorChoice").val();
+    var thirdColor = $("select#thirdColorChoice").val();
 
     $("#wrapper").css({
       "background": "linear-gradient(33deg, " + firstColor + ", " + secondColor + ", " + thirdColor + ")",
       "background-size": "1800% 1800%"
-      })
-
-    // document.getElementById("wrapper").css("background", function(index) {
-    //   return index = "linear-gradient(33deg, white, blue, black)"
-    // });
-
-  })
+    });
+  });
 });
